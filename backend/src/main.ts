82b27791e952021c691bab1f35b2695dae9579ef
Joppe Koers <joppe@joppekoers.nl>
2022-11-26 00:29:00 +0100

    Read port from env

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -63,6 +63,6 @@ async function bootstrap() {
 	app.useGlobalInterceptors(
 		new ClassSerializerInterceptor(app.get(Reflector)),
 	);
-	await app.listen(3000);
+	await app.listen(parseInt(process.env.PORT));
 }
 bootstrap();
